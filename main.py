a2c259341130b988b4920b2b1c36a199cd4523c3
ben-wycliff <benclifg@gmail.com>
2020-10-30 15:45:11 +0300

    Adds python command recognition script

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+from flask import Flask
 import speech_recognition as SR 
 import os
 
 print(SR.Microphone.list_microphone_names())
 
+commands = {
+    "code": "code",
+    "shutdown": "shutdown /p",
+    "matlab": "start matlab",
+    "pycharm": "start pycharm64",
+    "word": "start winword",
+    "photoshop": "start photoshop",
+    "excel": "start excel",
+    "access": "start msaccess",
+    "illustrator": "start illustrator",
+    "after effects": "start afterfx",
+    "control panel": "control panel",
+    "command prompt": "start cmd",
+    "cmd": "start cmd",
+    "task manager": "taskmgr",
+    "chrome": "start chrome"
+}
+
+app = Flask(__name__)
+@app.route('/' methods=['GET'])
 def recognize_speech():
     microphone = SR.Microphone()
     r = SR.Recognizer()
@@ -14,7 +35,7 @@ def recognize_speech():
         response = {"message": "", "success": True, "error": "No error"}
 
         try:
-            response["message"] = r.recognize_google(inputaudio)
+            response["message"] = r.recognize_google(inputaudio).lower()
         except SR.RequestError:
             # API was unreachable or unresponsive
             response["success"] = False
@@ -22,9 +43,11 @@ def recognize_speech():
         except SR.UnknownValueError:
             # speech was unintelligible
             response["error"] = "Unable to recognize speech"
-    print(response)
-    if "code"  in response["message"]:
-        os.system("code")
+    for i in commands:
+        # print(i)
+        if i in response["message"]:
+            print(f'opening {i}')
+            os.system(commands[i])
+            return '<h1>Success</h1>'
     return response
 
-recognize_speech()
